bc378ed96d14cdf4c8e9f8571c399c3d3d8c8cab
Geon Kim <jewbe22@gmail.com>
2023-01-19 10:10:34 -0500

    jUnit test

---
+++ project_test/src/project_test/unit_test_test.java
@@ -0,0 +1,14 @@
+package project_test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class unit_test_test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
