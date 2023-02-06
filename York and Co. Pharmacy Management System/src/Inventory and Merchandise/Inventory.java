bfaf0efbaea3286265f4fd9e7a359a0cf06f5379
Alvin-Ta <86690210+Alvin-Ta@users.noreply.github.com>
2023-02-05 21:34:22 -0500

    Added inventory class, medication class, and merchandise interface

---
+++ York and Co. Pharmacy Management System/src/Inventory and Merchandise/Inventory.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Inventory {
+    private static Inventory inventoryInstance;
+    private ArrayList<Merchandise> merchList = null;
+
+
+}
+
+//class HelloWorld {
+//    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//    }
+//}
