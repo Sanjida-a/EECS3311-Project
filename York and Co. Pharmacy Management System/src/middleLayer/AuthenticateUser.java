7d85459fc948db61b50f6f1ac7887f55955f561f
aiza-b <122501403+aiza-b@users.noreply.github.com>
2023-02-05 22:40:28 -0500

    merged Geon's GUI with previous commit;

--- York and Co. Pharmacy Management System/src/middleLayer/AuthenticateUser.java
+++ York and Co. Pharmacy Management System/src/middleLayer/AuthenticateUser.java
@@ -10,28 +10,30 @@ public class AuthenticateUser {
 //		this.num = 10;
 //	}
 //	
-	public static void checkUserValid(String username, char[] password) {
+	public static boolean checkUserValid(int username, int password) {
 		
-		System.out.println("TEST");
-		int usernameInt = Integer.parseInt(username);
-		System.out.println("Username: " + usernameInt);
+		System.out.println("TEST METHOD");
+//		int usernameInt = Integer.parseInt(username);
+		System.out.println("Username: " + username);
+		System.out.println("Username: " + password);
 //		String pass = Arrays.toString(password);
 //		int p = Integer.parseInt(new String(pass));
 //		System.out.println("pass " + pass);
 		
-		
+		// IF IT IS A VALID USERNAME+PASS, RETURN TRUE
+		// ELSE RETURN FALSE
         StringBuilder sb = new StringBuilder();
  
-        // Creating a string using append() method
-        for (int i = 0; i < password.length; i++) {
-            sb.append(password[i]);
-        }String pass = sb.toString();
-        System.out.println("pass " + pass);
-       
-        int passInteger = Integer.parseInt(pass);
-        System.out.println("pass " + passInteger);
-        
-        System.out.println("practice test");
+//        // Creating a string using append() method
+//        for (int i = 0; i < password.length; i++) {
+//            sb.append(password[i]);
+//        }String pass = sb.toString();
+//        System.out.println("pass " + pass);
+//       
+//        int passInteger = Integer.parseInt(pass);
+//        System.out.println("pass " + passInteger);
+        return true;
+//        System.out.println("practice test");
         
 	}
 
--- York and Co. Pharmacy Management System/src/presentation/DisplayLogin.java
+++ York and Co. Pharmacy Management System/src/presentation/DisplayLogin.java
@@ -11,11 +11,15 @@ import javax.swing.JButton;
 import java.awt.*;
 import java.awt.event.*;
 import middleLayer.*;
+import javax.swing.SwingConstants;
 
-public class DisplayLogin implements ActionListener {
-	private JTextField textField;
+public class DisplayLogin {
+	private JTextField userNameField;
 	private JPasswordField passwordField;
 	AuthenticateUser _authUser;
+
+    private static int username;
+	private static int password;
 //	public DisplayLogin() {};
 	
 //	public DisplayLogin(AuthenticateUser authUser) {
@@ -26,20 +30,19 @@ public class DisplayLogin implements ActionListener {
 
 	public void displayLogin() {
 		JFrame.setDefaultLookAndFeelDecorated(true);
-        JFrame frame = new JFrame("York and Co. Pharmacy Management System");
+        JFrame frmLogin = new JFrame("York and Co. Pharmacy Management System");
+        frmLogin.setTitle("Login");
         DisplayLogin background = new DisplayLogin();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setContentPane(background.createContentPanel());
-        frame.setSize(600, 400);
-        frame.setVisible(true);
+        frmLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frmLogin.setContentPane(background.createContentPanel(frmLogin));
+        frmLogin.setSize(600, 400);
+        frmLogin.setVisible(true);
 	}
 	
-	public JPanel createContentPanel() {
+	public JPanel createContentPanel(JFrame frame) {
 		JPanel totalGUI = new JPanel();
 		totalGUI.setLayout(null);
-		
-//<<<<<<< Updated upstream
-//=======
+
 		JLabel lblUserAccount = new JLabel("Username");
 		lblUserAccount.setFont(new Font("굴림", Font.BOLD, 20));
 		lblUserAccount.setBounds(100, 100, 125, 35);
@@ -50,20 +53,73 @@ public class DisplayLogin implements ActionListener {
 		lblPassword.setBounds(100, 150, 125, 35);
 		totalGUI.add(lblPassword);
 		
-		textField = new JTextField();
-		textField.setBounds(225, 100, 225, 35);
-		totalGUI.add(textField);
-		textField.setColumns(10);
+		// textField = new JTextField();
+		// textField.setBounds(225, 100, 225, 35);
+		// totalGUI.add(textField);
+		// textField.setColumns(10);
+		userNameField = new JTextField();
+		userNameField.setBounds(225, 100, 225, 35);
+		totalGUI.add(userNameField);
+		userNameField.setColumns(20);
+		userNameField.addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+//					if(validateInput(totalGUI, userNameField, passwordField)) {
+//						frame.dispose();
+//					}
+				}
+			}
+		});
 		
 		passwordField = new JPasswordField();
 		passwordField.setBounds(225, 150, 225, 35);
 		totalGUI.add(passwordField);
 		
-		JButton btnNewButton = new JButton("Login");
-		btnNewButton.addActionListener(this);
-		btnNewButton.setFont(new Font("굴림", Font.BOLD, 25));
-		btnNewButton.setBounds(225, 244, 150, 50);
-		totalGUI.add(btnNewButton);
+		// JButton btnNewButton = new JButton("Login");
+		// btnNewButton.addActionListener(this);
+		// btnNewButton.setFont(new Font("굴림", Font.BOLD, 25));
+		// btnNewButton.setBounds(225, 244, 150, 50);
+		// totalGUI.add(btnNewButton);
+		passwordField.setColumns(20);
+		passwordField.addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+//					if(validateInput(totalGUI, userNameField, passwordField)) {
+//						frame.dispose();
+//					}
+				}
+			}
+		});
+		
+
 		
 		JLabel lblTitle = new JLabel("York and Co. Pharmacy Management System");
 		lblTitle.setFont(new Font("굴림", Font.BOLD | Font.ITALIC, 20));
@@ -71,28 +127,126 @@ public class DisplayLogin implements ActionListener {
 		totalGUI.add(lblTitle);
 		
 		
-		totalGUI.setVisible(true);
-//>>>>>>> Stashed changes
-		return totalGUI;
-	}
 	
-	 public void actionPerformed(ActionEvent e) {
+	
+// 	 public void actionPerformed(ActionEvent e) {
 	    	
-    	String username = textField.getText();
-    	char[] pass = passwordField.getPassword();
-//    	System.out.println("TEST");
-//    	int usernameInt = Integer.parseInt(username);
-//    	int passwordInt = Integer.parseInt(pass);
+//     	String username = textField.getText();
+//     	char[] pass = passwordField.getPassword();
+// //    	System.out.println("TEST");
+// //    	int usernameInt = Integer.parseInt(username);
+// //    	int passwordInt = Integer.parseInt(pass);
     	
-    	//==_authUser.checkUserValid(username, pass);
-    	AuthenticateUser.checkUserValid(username, pass);
+//     	//==_authUser.checkUserValid(username, pass);
+//     	AuthenticateUser.checkUserValid(username, pass);
     	
     	
-	}
+// 	}
 //<<<<<<< Updated upstream
 
 //=======
 	
 	
 //>>>>>>> Stashed changes
+		
+		
+//		JButton btnNewButton = new JButton("Login");
+//		btnNewButton.addActionListener(this);
+//		btnNewButton.setFont(new Font("굴림", Font.BOLD, 25));
+//		btnNewButton.setBounds(225, 244, 150, 50);
+//		totalGUI.add(btnNewButton);
+
+		
+		
+		this.loginButton(totalGUI, frame);
+		
+		
+		totalGUI.setVisible(true);
+		return totalGUI;
+	}
+	
+	private void loginButton(JPanel totalGUI, JFrame frame) {
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setFont(new Font("굴림", Font.BOLD, 25));
+		btnLogin.setBounds(225, 244, 150, 50);
+		totalGUI.add(btnLogin);
+		btnLogin.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(validateInput(totalGUI, userNameField, passwordField)) {
+					Boolean inputIsFound = AuthenticateUser.checkUserValid(username, password);
+					if (inputIsFound==true) {
+						frame.dispose();
+					}
+				}
+			}
+		});
+
+	}
+	
+	private boolean validateInput(JPanel totalGUI, JTextField inputUsername, JPasswordField inputPassword) {
+		try {
+			username = Integer.getInteger(inputUsername.getText(), 0);
+			password = Integer.parseInt(new String(inputPassword.getPassword()));
+			return true;
+		}
+		catch(Exception e) {
+		
+			JPanel panelInvalidInput = new JPanel();
+			panelInvalidInput.setBounds(100, 195, 350, 35);
+			totalGUI.add(panelInvalidInput);
+			panelInvalidInput.setLayout(null);
+		
+			JLabel lblInvalidInput = new JLabel("username or password is invalid");
+			lblInvalidInput.setBounds(0, 0, 350, 35);
+			panelInvalidInput.add(lblInvalidInput);
+		
+			lblInvalidInput.setHorizontalAlignment(SwingConstants.CENTER);
+			lblInvalidInput.setForeground(new Color(255, 0, 0));
+			lblInvalidInput.setFont(new Font("굴림", Font.BOLD | Font.ITALIC, 20));
+			totalGUI.add(panelInvalidInput);
+			return false;
+		}
+	}
+	
+	
+	
+	public int getUsername() {
+		return username;
+	}
+	
+	public int getPassword() {
+		return password;
+	}
+	
+
+// 	 public void actionPerformed(ActionEvent e) {
+//	    
+//     	String username = userNameField.getText();
+//     	char[] pass = passwordField.getPassword();
+//     	System.out.println("TEST");
+//     	
+//     	if (username!= "1111") {
+//     		System.out.println("Incorrect User or Pass");
+//     	}
+//     	else {
+//     		//HOW TO CLOSE FRAME HERE??
+////     		dispose();
+//     	}
+// //    	int usernameInt = Integer.parseInt(username);
+// //    	int passwordInt = Integer.parseInt(pass);
+//    	
+//     	//==_authUser.checkUserValid(username, pass);
+// 		
+////     	AuthenticateUser.checkUserValid(username, pass);
+//    	
+//    	
+// 	}
+
+//	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		DisplayLogin login = new DisplayLogin();
+//		login.displayLogin();
+//	}
 }
