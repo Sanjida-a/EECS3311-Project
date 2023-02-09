e2ceb10f9e763df819c521bc1a59a67602984555
Geon Kim <jewbe22@gmail.com>
2023-02-08 23:23:41 -0500

    dropdown for search and minor bugfix

--- York and Co. Pharmacy Management System/src/middleLayer/MERCHANDISE_TYPE.java
+++ York and Co. Pharmacy Management System/src/middleLayer/MERCHANDISE_TYPE.java
@@ -9,9 +9,7 @@ public enum MERCHANDISE_TYPE {
 	SINUS;
 	
 	private static final MERCHANDISE_TYPE[] values = values(); 
-	public MERCHANDISE_TYPE next() {
-		return values[(ordinal() + 1) % values.length];
-	}
+
 	public static MERCHANDISE_TYPE getValue(String s) {
 		//MERCHANDISE_TYPE start = MERCHANDISE_TYPE.COUGH;
 		for(MERCHANDISE_TYPE t : values) {
