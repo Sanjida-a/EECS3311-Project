e721262239f3513a7973113145511bf4d7e1df5f
Geon Kim <jewbe22@gmail.com>
2023-02-13 16:35:07 -0500

    correction on the behaviour of ErrorPopup
    the popup is now closed by clicking Ok button only. the other screen is disabled until the popup is dismissed

--- York and Co. Pharmacy Management System/src/presentation/DisplayErrorPopup.java
+++ York and Co. Pharmacy Management System/src/presentation/DisplayErrorPopup.java
@@ -13,19 +13,27 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
+
 import java.awt.Dimension;
+import javax.swing.JPopupMenu;
 
 public class DisplayErrorPopup {
+
+
 	
-	public static void displayErrorPopup(String error) {
+	public static void displayErrorPopup(String error, JFrame superFrame) {
 		//creates frame
+		superFrame.setEnabled(false);
 		JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frmPopup = new JFrame("Error");
-        frmPopup.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frmPopup.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         JPanel panel_1 = new JPanel();
         frmPopup.setContentPane(panel_1);
         frmPopup.setSize(600, 400);
         frmPopup.getContentPane().setLayout(null);
+
+		
         
         JPanel panelError = new JPanel();
         panelError.setBounds(12, 28, 562, 88);
@@ -66,8 +74,11 @@ public class DisplayErrorPopup {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				frmPopup.dispose();
+				superFrame.setEnabled(true);
+				superFrame.toFront();
 			}
 		});
+
         frmPopup.setVisible(true);
 	}
 	
