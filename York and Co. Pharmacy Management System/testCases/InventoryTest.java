43eafa94baf81f7ff50a66da3b7f521ce7db4632
Alvin-Ta <86690210+Alvin-Ta@users.noreply.github.com>
2023-02-21 15:27:50 -0500

    Test classes were created

---
+++ York and Co. Pharmacy Management System/testCases/InventoryTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryTest {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void display() {
+    }
+
+    @Test
+    void increaseQuantity() {
+    }
+
+    @Test
+    void decreaseQuantity() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void addToInventory() {
+    }
+
+    @Test
+    void getMerchandise() {
+    }
+}
