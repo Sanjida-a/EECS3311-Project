09d2d389498540000f6ccc79dd324d20f8076c62
aiza-b <122501403+aiza-b@users.noreply.github.com>
2023-03-08 22:07:58 -0500

    Minor bug fixes

--- York and Co. Pharmacy Management System/src/testCases/IntegrationTests/ListOfOrdersTest.java
+++ York and Co. Pharmacy Management System/src/testCases/IntegrationTests/ListOfOrdersTest.java
@@ -3,6 +3,8 @@ package testCases.IntegrationTests;
 import middleLayer.*;
 import org.junit.jupiter.api.Test;
 
+import databaseDAO.superDAO;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,13 +12,20 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListOfOrdersTest {
-
+	
+	static String pass = "hello123";  // TA please change this according to your mySQL password in order for the tests to work
+	
     @Test
     void updateOrderListFromDatabase() {
     }
 
     @Test
     void getListOfAllOrders(){
+    	try {
+			superDAO.setPassword(pass);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 //        ListOfOrders L = ListOfOrders.getInstance();
 //        Order O = new Order(1, 1111122222, 1);
 //        Prescription p = new Prescription(1, 1111122222, 2);
