e673ca3a1ab06e20943ca6eeecfbdbf58d2d141d
Geon Kim <jewbe22@gmail.com>
2023-03-20 17:58:45 -0400

    fix for inventory unit test

--- York and Co. Pharmacy Management System/src/databaseDAO/MerchandiseData/MerchandiseStub.java
+++ York and Co. Pharmacy Management System/src/databaseDAO/MerchandiseData/MerchandiseStub.java
@@ -15,14 +15,22 @@ public class MerchandiseStub implements MerchandiseRoot {
 		Merchandise merc2 = new Merchandise(2, "pill2", 9, 3.00, MERCHANDISE_TYPE.COUGH, MERCHANDISE_FORM.TABLET, true, "description for piil2", true);
 		Merchandise merc3 = new Merchandise(3, "pill3", 8, 4.00, MERCHANDISE_TYPE.FEVER, MERCHANDISE_FORM.TABLET, false, "description for pill3", true);
 		Merchandise merc4 = new Merchandise(4, "pill4", 7, 5.00, MERCHANDISE_TYPE.SINUS, MERCHANDISE_FORM.LIQUID, false, null, true);
+		Merchandise merc5 = new Merchandise(5, "pill5", 6, 3.00, MERCHANDISE_TYPE.SINUS, MERCHANDISE_FORM.LIQUID, true, null, false);
 		allInventoryStub.add(merc1);
 		allInventoryStub.add(merc2);
 		allInventoryStub.add(merc3);
 		allInventoryStub.add(merc4);
+		allInventoryStub.add(merc5);
 	}
 	@Override
 	public ArrayList<Merchandise> getListOfMerchandise() {
-		return allInventoryStub;
+		ArrayList<Merchandise> result = new ArrayList<Merchandise>();
+		for(Merchandise m : allInventoryStub) {
+			if(m.isValid()) {
+				result.add(m);
+			}
+		}
+		return result;
 	}
 
 	@Override
