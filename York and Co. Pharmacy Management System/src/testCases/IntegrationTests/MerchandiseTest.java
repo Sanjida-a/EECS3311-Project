c5cb23edb0894147297a06ea5e8c8a88d0a703fd
aiza-b <122501403+aiza-b@users.noreply.github.com>
2023-04-04 15:15:17 -0400

    Added missing test classes

---
+++ York and Co. Pharmacy Management System/src/testCases/IntegrationTests/MerchandiseTest.java
@@ -0,0 +1,11 @@
+package testCases.IntegrationTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MerchandiseTest {
+
+	// no methods other than setters/getters to test
+
+}
---
+++ York and Co. Pharmacy Management System/src/testCases/IntegrationTests/PrescriptionTest.java
@@ -0,0 +1,11 @@
+package testCases.IntegrationTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PrescriptionTest {
+
+	// no methods other than setters/getters to test
+	
+}
